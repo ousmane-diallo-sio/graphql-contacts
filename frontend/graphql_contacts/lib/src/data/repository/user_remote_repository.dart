08639d68edb15bd972e77f7ec7bdda84e7e0e272
AladdineDev <aaldn.dev@gmail.com>
2024-09-12 17:06:52 +0200

    wip

--- frontend/graphql_contacts/lib/src/data/repository/user_remote_repository.dart
+++ frontend/graphql_contacts/lib/src/data/repository/user_remote_repository.dart
@@ -1,8 +1,12 @@
 import 'package:ferry/ferry.dart';
+import 'package:graphql_contacts/src/data/graphql/__generated__/read_user.data.gql.dart';
+import 'package:graphql_contacts/src/data/graphql/__generated__/read_user.req.gql.dart';
+import 'package:graphql_contacts/src/data/graphql/__generated__/read_user.var.gql.dart';
+import 'package:graphql_contacts/src/data/graphql/__generated__/read_users.data.gql.dart';
+import 'package:graphql_contacts/src/data/graphql/__generated__/read_users.req.gql.dart';
+import 'package:graphql_contacts/src/data/graphql/__generated__/read_users.var.gql.dart';
 import 'package:graphql_contacts/src/provider/ferry_client.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-// Importez les fichiers générés par ferry_codegen
 
 part 'user_remote_repository.g.dart';
 
@@ -31,44 +35,37 @@ class UserRemoteDataSource {
     required int limit,
     required int offset,
   }) {
-    final request = GUsersReq((b) {
-      b
+    final request = GUsersReq(
+      (b) => b
         ..vars.limit = limit
-        ..vars.offset = offset
-        ..vars.gender = gender
-        ..vars.city = city
-        ..vars.minHeight = minHeight
-        ..vars.maxHeight = maxHeight
-        ..vars.minWeight = minWeight
-        ..vars.maxWeight = maxWeight
-        ..vars.hasSocialNetworks = hasSocialNetworks;
-    });
+        ..vars.offset = offset,
+    );
     return ferryClient.request(request);
   }
 
-  Stream<OperationResponse<GUserCreateData, GUserCreateVars>> createUserStream({
-    required UserInput input,
-  }) {
-    final request = GCreateUserReq((b) => b..vars.input = input.toJson());
-    return ferryClient.request(request);
-  }
+  // Stream<OperationResponse<GUserCreateData, GUserCreateVars>> createUserStream({
+  //   required UserInput input,
+  // }) {
+  //   final request = GCreateUserReq((b) => b..vars.input = input.toJson());
+  //   return ferryClient.request(request);
+  // }
 
-  Stream<OperationResponse<GUserUpdateData, GUserUpdateVars>> updateUserStream({
-    required String id,
-    required UpdateUserInput input,
-  }) {
-    final request = GUpdateUserReq((b) {
-      b
-        ..vars.id = id
-        ..vars.input = input.toJson();
-    });
-    return ferryClient.request(request);
-  }
+  // Stream<OperationResponse<GUserUpdateData, GUserUpdateVars>> updateUserStream({
+  //   required String id,
+  //   required UpdateUserInput input,
+  // }) {
+  //   final request = GUpdateUserReq((b) {
+  //     b
+  //       ..vars.id = id
+  //       ..vars.input = input.toJson();
+  //   });
+  //   return ferryClient.request(request);
+  // }
 
-  Stream<OperationResponse<GUserDeleteData, GUserDeleteVars>> deleteUserStream({
-    required String id,
-  }) {
-    final request = GDeleteUserReq((b) => b..vars.id = id);
-    return ferryClient.request(request);
-  }
+  // Stream<OperationResponse<GUserDeleteData, GUserDeleteVars>> deleteUserStream({
+  //   required String id,
+  // }) {
+  //   final request = GDeleteUserReq((b) => b..vars.id = id);
+  //   return ferryClient.request(request);
+  // }
 }
