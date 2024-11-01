b19d3dd46f4261f348d739912917ae8c3f8f8403
AladdineDev <aaldn.dev@gmail.com>
2024-11-01 18:45:53 +0100

    refactor: gap

--- frontend/graphql_contacts/lib/src/routing/not_found_screen.dart
+++ frontend/graphql_contacts/lib/src/routing/not_found_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_contacts/src/constants/sizes.dart';
 import 'package:graphql_contacts/src/utils/build_context_extension.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:graphql_contacts/src/routing/router.dart';
 
 class NotFoundScreen extends ConsumerWidget {
   const NotFoundScreen({super.key});
-
-  // Define the gap as a widget with a constant size
-  Widget get gap32 => const SizedBox(height: 32);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,8 +19,7 @@ class NotFoundScreen extends ConsumerWidget {
               style: context.textTheme.headlineMedium,
               textAlign: TextAlign.center,
             ),
-            gap32, // Ensure gap32 returns a SizedBox
-            // Use ElevatedButton instead of FilledButton if there is an issue with FilledButton
+            gap32,
             ElevatedButton(
               onPressed: () => const HomeScreenRoute().go(context),
               child: const Text("Retourner à l'accueil"),
