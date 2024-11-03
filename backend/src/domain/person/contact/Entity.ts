41f50d68542a4c727d33d97acd065da3bb1e6216
DIALLO <ousmane.diallo1.sio@gmail.com>
2024-11-03 17:07:12 +0100

    tmp commit

--- backend/src/domain/person/contact/Entity.ts
+++ backend/src/domain/person/contact/Entity.ts
@@ -1,17 +1,11 @@
-import { BeforeCreate, BeforeUpdate, BeforeUpsert, Embedded, Entity, Enum, type EventArgs, Property, Rel, Unique } from '@mikro-orm/core';
-import { BaseEntity } from '../../../db/BaseEntity';
-import jwt from "jsonwebtoken";
-import EnvConfig from '../../../lib/config/EnvConfig';
-import crypto from "crypto";
-import { Address } from '../../address/Entity';
-import { SocialNetworks } from '../../social-networks/Entity';
+import { Entity, ManyToOne, type Rel, Unique } from '@mikro-orm/core';
 import { Person } from '../Entity';
 import { User } from '../user/Entity';
 
 @Entity()
-@Unique({ properties: ['email'] })
 export class Contact extends Person {
 
+  @ManyToOne()
   referal!: Rel<User>;
 
 }
